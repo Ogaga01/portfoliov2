1e0fa02347a6a2c3c1f8f4fa6acf7812855dea46
Ogaga <iyaraogaga69@gmail.com>
2023-02-28 11:49:30 +0100

    Add Files

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import MobileNavbar from './components/MobileNavbar';
+import Navbar from './components/Navbar';
+import Project from './components/Project';
+import Recommendation from './components/Recommendation';
+import Skills from './components/Skills';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MobileNavbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Project />
+      <Recommendation />
+      <Contact />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Project.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import { projects } from '../types/projects';
+import ProjectCard from './ProjectCard';
+import styles from './../sass/_project.module.scss'
+
+const Project: FC = () => {
+    return (
+      // <Fade bottom>
+        <section id='projects' className={styles["projects"]}>
+          <h1 className={styles["projects__head"]}>My Featured Projects</h1>
+          <div className={styles["projects__div"]}>
+            {projects.map((poject) => (
+              <ProjectCard props={poject} />
+            ))}
+          </div>
+        </section>
+      // {/* </Fade> */}
+    );
+};
+
+export default Project;
